1f0193a1e1510cae293060d20b7702ffa86430e9
PJHRobles <joshuahrobles@gmail.com>
2018-05-24 13:29:53 +0800

    Change order of symlink on files

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ set -euvx
 DIR=`pwd`
 ZSH_DIR=~/.oh-my-zsh/
 
-ln -sf ${DIR}/zsh/bullet-train.zsh-theme ${ZSH_DIR}/themes/bullet-train.zsh-theme
-ln -sf ${DIR}/zsh/zshrc ~/.zshrc
+
 ln -sf ${DIR}/vim/vimrc ~/.vimrc
 ln -sf ${DIR}/git/gitconfig ~/.gitconfig
+ln -sf ${DIR}/zsh/bullet-train.zsh-theme ${ZSH_DIR}/themes/bullet-train.zsh-theme
+ln -sf ${DIR}/zsh/zshrc ~/.zshrc
