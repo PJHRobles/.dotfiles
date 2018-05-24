5f024c4c597745cec7d8afc415d277b9432db6e7
PJHRobles <joshuahrobles@gmail.com>
2018-05-24 11:24:48 +0800

    Add zsh custom theme

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-#! /bin/sh
+#! /bin/zsh
 
-set -euvx 
+set -euvx
 
 # straightforward install for now and convert to functions later
 DIR=`pwd`
+ZSH_DIR=~/.oh-my-zsh/
 
+ln -sf ${DIR}/zsh/bullet-train.zsh-theme ${ZSH_DIR}/themes/bullet-train.zsh-theme
 ln -sf ${DIR}/zsh/zshrc ~/.zshrc
 ln -sf ${DIR}/vim/vimrc ~/.vimrc
 ln -sf ${DIR}/git/gitconfig ~/.gitconfig
